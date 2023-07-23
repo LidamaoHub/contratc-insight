aa765c03720f2ac68624008f097691eec9b6e8dd
Tiyo <tiyosheng@gmail.com>
2023-07-23 20:31:16 +0800

    update: rem to px

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import './assets/css/reset.css'
 import 'vant/lib/index.css'
 import './style.scss'
-import './assets/js/rem'
+// import './assets/js/rem'
 import { Loading, Collapse, CollapseItem, Dialog, Popover, Circle } from 'vant'
 
 createApp(App).use(Loading).use(Collapse).use(CollapseItem).use(Dialog).use(Popover).use(Circle).mount('#app')
